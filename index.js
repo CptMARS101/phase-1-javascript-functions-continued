5410203c315d0fe0584b2f962baca6551dc53f28
Marcus Kitchen <marcusdkitchen@gmail.com>
2023-11-15 10:11:59 -0500

    funct cont

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+function saturdayFun(activity) {
+    let fun = activity || "roller-skate"
+    return `This Saturday, I want to ${fun}!`
+}
+saturdayFun("bathe my dog");
+
+function mondayWork(task) {
+    let activity = task || "go to the office";
+    return `This Monday, I will ${activity}.`
+}
+mondayWork("work from home");
+
+function wrapAdjective(flair) {
+    let wrap = flair || "*";
+  return function(adjective = "special") {
+return `You are ${wrap}${adjective}${wrap}!`
+}
+}
